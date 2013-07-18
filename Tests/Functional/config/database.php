6225d732bdb87f16eb5d4df772166f798a5bbffe
vitiko <vitiko@mail.ru>
2013-07-18 16:08:41 +0400

    sqllite for tests

--- Tests/Functional/config/database.php
+++ Tests/Functional/config/database.php
@@ -3,13 +3,14 @@
 $path = $container->getParameter ('kernel.test_env_dir').'/db.sqllite';
 
 
-/*$container->loadFromExtension('doctrine', array(
+$container->loadFromExtension('doctrine', array(
     'dbal' => array(
         'driver' => 'pdo_sqlite',
         'path' => $path,
     ),
-));*/
+));
 
+/*
 $container->loadFromExtension('doctrine', array(
     'dbal' => array(
         'driver' => 'pdo_mysql',
@@ -19,3 +20,4 @@ $container->loadFromExtension('doctrine', array(
         'dbname' => 'iphptree_test'
     ),
 ));
+*/
