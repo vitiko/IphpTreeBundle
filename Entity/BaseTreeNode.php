8965ee4ce51a05818a52b02e4220f169748d0c76
vitiko <vitiko@mail.ru>
2014-06-05 11:44:59 +0400

    add BaseTreeNode

---
+++ Entity/BaseTreeNode.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Vitiko <vitiko@mail.ru>
+ */
+
+namespace Iphp\TreeBundle\Entity;
+use Iphp\TreeBundle\Model\TreeNode as ModelTreeNode;
+
+class BaseTreeNode extends ModelTreeNode {
+
+} 
